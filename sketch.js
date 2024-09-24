022d95305f49b4e3c37361e8536ad8236b177aa9
potuitou <qpeng@risd.edu>
2024-09-23 20:53:24 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,37 +1,53 @@
 let angle = -1;
-let a = 0.5; 
-let aSwitch = -1; 
+let a = 0.5;
+let aSwitch = -1;
+let d = 0;
+let dSwitch = -1;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
+  noFill();
+  for (let d = 0; d < windowWidth; d += 15) {
+    ellipse(windowWidth / 2, windowHeight / 2, d);
+  }
+
+  frameRate(25);
 }
 
 function draw() {
+  background(230, 208, 255, 5);
 
-  background(255,255,255,5);
+  if (d >= windowWidth || d <= 0) {
+    dSwitch *= -1;
+  }
+
+  d += 15 * dSwitch;
+
+  strokeWeight(1);
+  stroke(255,255,255,100);
+  ellipse(windowWidth / 2, windowHeight / 2, d);
 
   if (a >= 1 || a <= 0) {
     aSwitch *= -1;
   }
 
-  a += 0.01*aSwitch;
-  
+  // a += 0.01*aSwitch;
+  a += 0.01;
+
   translate(windowWidth / 2, windowHeight / 2);
   rotate(a);
 
-
   stroke(255, 204, 0);
-  star(0, 0, 350, 10,2);
+  star(0, 0, 350, 10, 2);
 
   stroke(0);
-  star(0, 0, 300, 30,2);
-  star(0, 0, 20, 70,2);
-  star(0, 0, 250, 50,2);
-
-
+  star(0, 0, 300, 30, 2);
+  star(0, 0, 20, 70, 2);
+  star(0, 0, 250, 50, 2);
 }
 
-function star(x, y, s, c,sw) {
+function star(x, y, s, c, sw) {
   strokeWeight(sw);
   line(x - s / 2, y, x - c / 2, y + c / 2);
   line(x - c / 2, y + c / 2, x, y + s / 2);
